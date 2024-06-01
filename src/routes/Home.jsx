2daad08965483814d115e92f2429d8a893eba299
Suyel Bishnu <s2301242@BCVFVFLU1WG7Air.home>
2024-06-01 22:52:57 +0300

    Add bootstrap styling

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,15 +1,14 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import '../components/css/home.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import "../components/css/home.css";
 
 const Home = () => {
   return (
-    <div>
-      <Link to="/animals">Animals</Link>
+    <div className="homePage">
+      <Link to="/animals">Animal</Link>
       <Link to="/birds">Birds</Link>
       <Link to="/insects">Insects</Link>
       <Link to="/fishes">Fishes</Link>
-
     </div>
   );
 };
