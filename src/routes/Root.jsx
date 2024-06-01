f450c2d64b43bf7fb5ea2a2c607d615256be6a8b
Suyel Bishnu <s2301242@BCVFVFLU1WG7Air.home>
2024-06-01 22:53:29 +0300

    Add bootstrap for styling

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import Header from '../components/Header';
-import Footer from '../components/Footer';
-import { Outlet } from 'react-router-dom'; 
+import React from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import { Outlet } from "react-router-dom";
 
 const Root = () => {
   return (
     <>
-<Header/>
-<main>
-    <Outlet/>
-    </main>
-<Footer/>
-</>
+      <Header />
+      <main className="container">
+        <Outlet />
+      </main>
+      <Footer />
+    </>
   );
 };
 
