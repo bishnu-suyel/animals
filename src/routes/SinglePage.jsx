f389174b10921279c0112b3906a517b8b86d981a
Suyel Bishnu <s2301242@BCVFVFLU1WG7Air.home>
2024-06-01 23:50:48 +0300

    Add api to fetch wikipedia info

--- src/routes/SinglePage.jsx
+++ src/routes/SinglePage.jsx
@@ -1,17 +1,49 @@
-import React from 'react'
-import {useParams} from "react-router-dom";
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 
-   const SinglePage = ({...rest}) => {
-    const params = useParams();
-    
-    const categoryItems = rest[params.category];
+const SinglePage = ({ ...rest }) => {
+  const params = useParams();
 
-    const data = categoryItems.find(el => el. name === params.name);
+  const categoryItems = rest[params.category];
+
+  const data = categoryItems.find((el) => el.name === params.name);
+  const [wikiInfo, setWikiInfo] = useState({});
+
+  useEffect(() => {
+    const fetchWikiInfo = async () => {
+      try {
+        const response = await axios.get(
+          `https://en.wikipedia.org/api/rest_v1/page/summary/${data.name}`
+        );
+        setWikiInfo(response.data);
+      } catch (error) {
+        console.error("Error fetching Wikipedia data:", error);
+        setWikiInfo({ extract: "Information not available." });
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchWikiInfo();
+  }, [data.name]);
   return (
-  <>
-  <h2>{data.name}</h2>
-  </>
+    <>
+      <div className="container mt-5 px-5">
+        <h2 className="mb-4 fs-5">{wikiInfo.title}</h2>
+        <div>
+          <p className="fs-6">{wikiInfo.extract}</p>
+          <a
+            href={wikiInfo.content_urls?.desktop?.page || "#"}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="fs-6"
+          >
+            Read more on Wikipedia
+          </a>
+        </div>
+      </div>
+    </>
   );
 };
 export default SinglePage;
