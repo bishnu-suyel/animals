7b945866e6ddfd945c41c35b54624fe7419e414c
Suyel Bishnu <s2301242@BCVFVFLU1WG7Air.home>
2024-06-01 22:49:17 +0300

    Add button and bootstrap styling

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,24 +1,59 @@
-import React from 'react';
-import './css/card.css';
-import { Link } from 'react-router-dom';
+import React from "react";
+// import './css/card.css';
+import { Link } from "react-router-dom";
+import "./css/main.css";
 
-const Card = ({name,likes, addLikes, removeCard, removeLikes}) => {
+const Card = ({ name, likes, addLikes, removeCard, removeLikes }) => {
   return (
-    <div className='card'>
-        <h2>{name}</h2>
-        <img src={`https://source.unsplash.com/400x400/?${name}`} />
-        <button className='close btn' onClick={removeLikes}>
-          <span className="material-symbols-outlined">heart_minus</span>
-        </button>
-        <p>{likes}</p>
-        <button className='btn' onClick={removeCard}>
-          <span className="material-symbols-outlined">cancel</span>
-        </button>
-        <button  className='likes' onClick={addLikes}>
-          <span className="material-symbols-outlined">heart_plus</span>
-        </button>
-       <Link to={name}>See more</Link>
+    <>
+      <div className="card col-3">
+        <button
+          type="button"
+          className="btn-close"
+          aria-label="Close"
+          onClick={removeCard}
+        ></button>
+
+        <img
+          src={`https://source.unsplash.com/400x400/?${name}`}
+          className="card-img-top"
+          alt="..."
+        />
+        <div className="card-body">
+          <h5 className="card-title text-capitalize">{name}</h5>
+          <div className="card-text">
+            <div className=" row">
+              <span
+                className="material-symbols-outlined heart-btn col"
+                onClick={addLikes}
+              >
+                heart_plus
+              </span>
+              <div className="col">
+                <span className="material-symbols-outlined">
+                  favorite
+                  <span className="fs-6">{likes}</span>
+                </span>
+              </div>
+
+              <span
+                className="heart-btn material-symbols-outlined col"
+                onClick={removeLikes}
+              >
+                heart_minus
+              </span>
+            </div>
+          </div>
+          <Link
+            className="btn btn-prima
+        ery"
+            to={name}
+          >
+            See more
+          </Link>
         </div>
+      </div>
+    </>
   );
 };
 
