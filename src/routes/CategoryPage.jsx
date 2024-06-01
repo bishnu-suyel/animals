6117eb1a987cb43ea2b89dd5ccfe3ce494a53297
Suyel Bishnu <s2301242@BCVFVFLU1WG7Air.home>
2024-06-01 22:52:28 +0300

    Add bootstrap styling

--- src/routes/CategoryPage.jsx
+++ src/routes/CategoryPage.jsx
@@ -2,25 +2,27 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import Card from "../components/Card";
 
-
-
-const CategoryPage = ({removeCard, removeLikes, addLikes, ...rest}) => {
+const CategoryPage = ({ removeCard, removeLikes, addLikes, ...rest }) => {
   const { category } = useParams();
-  
- const categoryItems = rest[category];
+
+  const categoryItems = rest[category];
   return (
     <>
-      <h2>{category}</h2>
-      {categoryItems.map((item)=>{
-        return <Card 
-        key = {item.name} 
-        name={item.name} 
-        likes={item.likes}
-        removeCard={()=>removeCard(item.name, category)} 
-        removeLikes={()=>removeLikes(item.name, category, 'remove')} 
-        addLikes={()=>addLikes(item.name, category, 'add')}
-        />;
-      })}
+      <h2 className="text-capitalize">{category}</h2>
+      <div className="container row gap-3">
+        {categoryItems.map((item) => {
+          return (
+            <Card
+              key={item.name}
+              name={item.name}
+              likes={item.likes}
+              removeCard={() => removeCard(item.name, category)}
+              removeLikes={() => removeLikes(item.name, category, "remove")}
+              addLikes={() => addLikes(item.name, category, "add")}
+            />
+          );
+        })}
+      </div>
     </>
   );
 };
