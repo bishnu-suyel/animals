26cab381b08ee9a8616f4698854e5ae6eb0608af
Suyel Bishnu <s2301242@BCVFVFLU1WG7Air.home>
2024-06-03 21:29:13 +0300

    Add images to the homepage

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -4,11 +4,52 @@ import "../components/css/home.css";
 
 const Home = () => {
   return (
-    <div className="homePage">
-      <Link to="/animals">Animal</Link>
-      <Link to="/birds">Birds</Link>
-      <Link to="/insects">Insects</Link>
-      <Link to="/fishes">Fishes</Link>
+    <div className="container">
+      <h2 className="text-center mb-4">Categories</h2>
+      <div className="row justify-content-center">
+        <div className="col-md-6 mb-4 d-flex justify-content-center">
+          <Link to="/animals" className="text-decoration-none">
+            <img
+              src="https://source.unsplash.com/300x300/?animals"
+              className="category-img img-fluid"
+              alt="Animals"
+            />
+            <h4 className="text-center mt-2">Animals</h4>
+          </Link>
+        </div>
+        <div className="col-md-6 mb-4 d-flex justify-content-center">
+          <Link to="/birds" className="text-decoration-none">
+            <img
+              src="https://source.unsplash.com/300x300/?birds"
+              className="category-img img-fluid"
+              alt="Birds"
+            />
+            <h4 className="text-center mt-2">Birds</h4>
+          </Link>
+        </div>
+      </div>
+      <div className="row justify-content-center">
+        <div className="col-md-6 mb-4 d-flex justify-content-center">
+          <Link to="/insects" className="text-decoration-none">
+            <img
+              src="https://source.unsplash.com/300x300/?insects"
+              className="category-img img-fluid"
+              alt="Insects"
+            />
+            <h4 className="text-center mt-2">Insects</h4>
+          </Link>
+        </div>
+        <div className="col-md-6 mb-4 d-flex justify-content-center">
+          <Link to="/fishes" className="text-decoration-none">
+            <img
+              src="https://source.unsplash.com/300x300/?fishes"
+              className="category-img img-fluid"
+              alt="Fishes"
+            />
+            <h4 className="text-center mt-2">Fishes</h4>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
