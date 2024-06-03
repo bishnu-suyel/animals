71eec38b86437879978884ed6ee1bbe18ce59f2d
Suyel Bishnu <s2301242@BCVFVFLU1WG7Air.home>
2024-06-03 21:30:02 +0300

    Import footer to the single page

--- src/routes/SinglePage.jsx
+++ src/routes/SinglePage.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import Footer from "../components/Footer";
 import axios from "axios";
 
 const SinglePage = ({ ...rest }) => {
