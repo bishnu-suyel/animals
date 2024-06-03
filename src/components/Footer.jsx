f20ce0aaf1303dcfc3cda3b09e4595c3fab15bd0
Suyel Bishnu <s2301242@BCVFVFLU1WG7Air.home>
2024-06-03 21:27:29 +0300

    Add new styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="mt-3 mb-3 d-flex justify-content-center">
-      <small>Copyrights &copy; Bishnu Suyel</small>
+    <footer className=" mt-3 mb-3 d-flex justify-content-center bg-light">
+      <small>Copyrights &copy; Bishnu Suyel.</small>
     </footer>
   );
 };
