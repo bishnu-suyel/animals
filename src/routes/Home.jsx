07b69182e94b2dff4d88047267124a9d3207c2ec
Suyel Bishnu <s2301242@BCVFVFLU1WG7Air.home>
2024-06-04 01:04:03 +0300

    Add bootstrap styling

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,27 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "../components/css/home.css";
+// import "../components/css/home.css";
 
 const Home = () => {
   return (
     <div className="container">
       <h2 className="text-center mb-4">Categories</h2>
       <div className="row justify-content-center">
-        <div className="col-md-6 mb-4 d-flex justify-content-center">
+        <div className="col-md-6 mb-4 d-flex justify-content p-4">
           <Link to="/animals" className="text-decoration-none">
             <img
-              src="https://source.unsplash.com/300x300/?animals"
-              className="category-img img-fluid"
+              src="https://source.unsplash.com/600x350/?animals"
+              className="category-img img-fluid - border border-success border-2"
               alt="Animals"
             />
             <h4 className="text-center mt-2">Animals</h4>
           </Link>
         </div>
-        <div className="col-md-6 mb-4 d-flex justify-content-center">
+        <div className="col-md-6 mb-4 d-flex justify p-4">
           <Link to="/birds" className="text-decoration-none">
             <img
-              src="https://source.unsplash.com/300x300/?birds"
-              className="category-img img-fluid"
+              src="https://source.unsplash.com/600x350/?birds"
+              className="category-img img-fluid - border border-success border-2"
               alt="Birds"
             />
             <h4 className="text-center mt-2">Birds</h4>
@@ -29,21 +29,21 @@ const Home = () => {
         </div>
       </div>
       <div className="row justify-content-center">
-        <div className="col-md-6 mb-4 d-flex justify-content-center">
+        <div className="col-md-6 mb-4 d-flex justify-content-center p-4">
           <Link to="/insects" className="text-decoration-none">
             <img
-              src="https://source.unsplash.com/300x300/?insects"
-              className="category-img img-fluid"
+              src="https://source.unsplash.com/600x350/?insects"
+              className="category-img img-fluid - border border-success border-2"
               alt="Insects"
             />
             <h4 className="text-center mt-2">Insects</h4>
           </Link>
         </div>
-        <div className="col-md-6 mb-4 d-flex justify-content-center">
+        <div className="col-md-6 mb-4 d-flex justify-content-center p-4">
           <Link to="/fishes" className="text-decoration-none">
             <img
-              src="https://source.unsplash.com/300x300/?fishes"
-              className="category-img img-fluid"
+              src="https://source.unsplash.com/600x350/?fishes"
+              className="category-img img-fluid - border border-success border-2"
               alt="Fishes"
             />
             <h4 className="text-center mt-2">Fishes</h4>
