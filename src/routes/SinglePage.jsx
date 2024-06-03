129529aa52e4fbd486ab18aca7970e4e0915b907
Suyel Bishnu <s2301242@BCVFVFLU1WG7Air.home>
2024-06-04 01:05:20 +0300

    Remove import footer

--- src/routes/SinglePage.jsx
+++ src/routes/SinglePage.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import Footer from "../components/Footer";
 import axios from "axios";
 
 const SinglePage = ({ ...rest }) => {
