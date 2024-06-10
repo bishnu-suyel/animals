e47c45a1b620f3a3e7eb8e90108faebb63e0625a
Suyel Bishnu <s2301242@BCVFVFLU1WG7Air.home>
2024-06-10 23:53:15 +0300

    Add footer.css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import "../components/css/footer.css";
 
 
 const Footer = () => {
   return (
-    <footer className=" mt-3 mb-3 d-flex justify-content-center bg-light">
+    <footer className="fixed-bottom mt-3 mb-3 d-flex justify-content-center bg-light">
       <small>Copyrights &copy; Bishnu Suyel {new Date().getFullYear()}</small>
     </footer>
   );
