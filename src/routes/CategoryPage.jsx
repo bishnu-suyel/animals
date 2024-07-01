c5b3a4f2e068298b5914c63de0ff5440a41ebfad
Suyel Bishnu <s2301242@BCVFVFLU1WG7Air.home>
2024-07-01 14:54:30 +0300

    Add search functionality

--- src/routes/CategoryPage.jsx
+++ src/routes/CategoryPage.jsx
@@ -1,30 +1,87 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import Card from "../components/Card";
 
-const CategoryPage = ({ removeCard, removeLikes, addLikes, ...rest }) => {
+const CategoryPage = ({
+  animals,
+  birds,
+  fishes,
+  insects,
+  removeCard,
+  removeLikes,
+  addLikes,
+}) => {
   const { category } = useParams();
+  const [search, setSearch] = useState("");
 
-  const categoryItems = rest[category];
-  return (
-    <>
-      <h2 className="text-capitalize">{category}</h2>
-      <div className="container row gap-3">
-        {categoryItems.map((item) => {
-          return (
-            <Card
-              key={item.name}
-              name={item.name}
-              likes={item.likes}
-              removeCard={() => removeCard(item.name, category)}
-              removeLikes={() => removeLikes(item.name, category, "remove")}
-              addLikes={() => addLikes(item.name, category, "add")}
-            />
-          );
-        })}
-      </div>
-    </>
+  const categoryItems = getCategoryItems(
+    category,
+    animals,
+    birds,
+    fishes,
+    insects
   );
+
+  const searchHandler = (event) => {
+    setSearch(event.target.value);
+  };
+
+  return (
+    <div className="container mt-4">
+      <div className="d-flex justify-content-end mb-4">
+        <div className="input-group" style={{ width: "200px" }}>
+          <input
+            id="searchbar"
+            type="text"
+            className="form-control form-control-sm"
+            placeholder="Search..."
+            onChange={searchHandler}
+            value={search}
+          />
+          <button
+            className="btn btn-outline-secondary"
+            type="button"
+            onClick={() => setSearch("")}
+          >
+            Reset
+          </button>
+        </div>
+      </div>
+      <div className="row">
+        {categoryItems
+          .filter((el) =>
+            el.name.toLowerCase().startsWith(search.toLowerCase())
+          )
+          .map((item) => (
+            <div key={item.name} className="col-md-4 mb-4">
+              <Card
+                name={item.name}
+                likes={item.likes}
+                removeCard={() => removeCard(item.name, category)}
+                removeLikes={() => removeLikes(item.name, category, "remove")}
+                addLikes={() => addLikes(item.name, category, "add")}
+                className="card cursor-pointer"
+              />
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+};
+
+const getCategoryItems = (category, animals, birds, fishes, insects) => {
+  switch (category) {
+    case "animals":
+      return animals || [];
+    case "birds":
+      return birds || [];
+    case "fishes":
+      return fishes || [];
+    case "insects":
+      return insects || [];
+    default:
+      return [];
+  }
 };
 
 export default CategoryPage;
