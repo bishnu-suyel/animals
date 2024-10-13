d863de29e9cf64f42011afb5304c43d02a25e375
Suyel Bishnu <suyel2007@gmail.com>
2024-10-13 19:49:01 +0300

    Add use effect &  axios to fetch images

--- src/routes/SinglePage.jsx
+++ src/routes/SinglePage.jsx
@@ -9,6 +9,7 @@ const SinglePage = ({ ...rest }) => {
   const categoryItems = rest[params.category];
   const data = categoryItems.find((el) => el.name === params.name);
   const [wikiInfo, setWikiInfo] = useState({});
+  const [imageUrl, setImageUrl] = useState("");
 
   useEffect(() => {
     const fetchWikiInfo = async () => {
@@ -22,7 +23,29 @@ const SinglePage = ({ ...rest }) => {
       }
     };
 
+    const fetchImage = async () => {
+      try {
+        const response = await axios.get(
+          `https://api.unsplash.com/search/photos?query=${
+            data.name
+          }&per_page=1&client_id=${import.meta.env.VITE_UNSPLASH_ACCESS_KEY}`
+        );
+
+        if (response.data.results.length > 0) {
+          setImageUrl(response.data.results[0].urls.regular);
+        } else {
+          console.warn(
+            `No images found for ${data.name}. Using fallback image.`
+          );
+          setImageUrl(`https://source.unsplash.com/600x600/?${data.name}`);
+        }
+      } catch (error) {
+        console.error(`Error fetching image for ${data.name}:`, error);
+      }
+    };
+
     fetchWikiInfo();
+    fetchImage(); // Fetch the image based on the name
   }, [data.name]);
 
   const handleBackClick = () => {
@@ -30,25 +53,31 @@ const SinglePage = ({ ...rest }) => {
   };
 
   return (
-    <>
-      <div className="container mt-5 px-5">
-        <h2 className="mb-4 fs-5">{wikiInfo.title}</h2>
-        <div>
-          <p className="fs-6">{wikiInfo.extract}</p>
-          <a
-            href={wikiInfo.content_urls?.desktop?.page || "#"}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="fs-6"
-          >
-            Read more on Wikipedia
-          </a>
-        </div>
-        <button onClick={handleBackClick} className="btn btn-primary mt-4">
-          Back to Previous Page
-        </button>
+    <div className="container mt-5 px-5">
+      <h2 className="mb-4 fs-5">{wikiInfo.title}</h2>
+      <div className="text-center">
+        <img
+          src={imageUrl || `https://source.unsplash.com/600x600/?${data.name}`}
+          alt={data.name}
+          className="img-fluid mb-4" 
+          style={{ width: "400px", height: "300px", objectFit: "cover" }} 
+        />
       </div>
-    </>
+      <div>
+        <p className="fs-6">{wikiInfo.extract}</p>
+        <a
+          href={wikiInfo.content_urls?.desktop?.page || "#"}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="fs-6"
+        >
+          Read more on Wikipedia
+        </a>
+      </div>
+      <button onClick={handleBackClick} className="btn btn-primary mt-4">
+        Back to Previous Page
+      </button>
+    </div>
   );
 };
 
